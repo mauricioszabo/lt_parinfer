e9dc29c2c4ac8c2f7484c095b0d400502fd10232
Maurício Szabo <mauricio.szabo@gmail.com>
2016-03-24 18:35:27 -0300

    Added multi-mode.

--- src/lt/plugins/lt_parinfer.cljs
+++ src/lt/plugins/lt_parinfer.cljs
@@ -5,7 +5,8 @@
             [lt.objs.statusbar :as stat]
             [lt.objs.popup :as popup]
             [lt.objs.editor.pool :as pool]
-            [lt.objs.command :as cmd])
+            [lt.objs.command :as cmd]
+            [crate.binding :refer [bound]])
   (:require-macros [lt.macros :refer [behavior]]))
 
 (def pi (js/require (plugins/local-module "Parinfer" "parinfer")))
@@ -26,7 +27,8 @@
         history (.getHistory cm)
         cursor-line (.-line cursor)
         cursor-x (-> cursor .-ch inc)
-        result (run-parinfer old-txt cursor-line cursor-x)
+        result (run-parinfer old-txt cursor-line cursor-x
+                             (get-in @parinfer-editors [:modes (editor-id)]))
         txt (.-text result)]
     (if (not= old-txt txt)
       (do
@@ -35,24 +37,95 @@
         (.setCursor cm cursor)
         (.setHistory cm history)))))
 
-(cmd/command {:command :parinfer-behind-cursor
-              :desc "Parinfer: infer parenthesis on current editor"
-              :exec parinfer-behind-cursor})
+(defn- editor-id [] (object/->id (pool/last-active)))
 
-(defn show-on-statusbar []
-  (let [ed (pool/last-active)]
-    (println "Current editor is in mode" (or (get modes ed) "no-parinfer"))))
+(defn ->mode-str [{:keys [modes current-editor]}]
+  (let [mode (get modes current-editor)]
+    (case mode
+      :indent [:span.pos "Parinfer: indent mode"]
+      :paren [:span.pos "Parinfer: paren mode"]
+      :disabled [:span.pos "Parinfer: disabled"]
+      [:span.pos])))
 
 (behavior ::show-parinfer-mode
           :triggers #{:active}
-          :reaction (fn [obj] (show-on-statusbar)))
+          :reaction (fn [obj]))
+
+(object/object* ::parinfer.editors
+                :triggers #{}
+                :behaviors #{::show-parinfer-mode}
+                :modes {}
+                :current-editor nil
+                :init (fn [this]
+                        (stat/statusbar-item (bound this ->mode-str) "")))
+
+(def parinfer-editors (object/create ::parinfer.editors))
+(stat/add-statusbar-item parinfer-editors)
+
+(defn update-editor-and-state [ed cm result]
+  (object/assoc-in! parinfer-editors [:modes (editor-id)] :indent)
+  (if (not= (.-text result) (editor/->val ed))
+    (editor/set-val cm (.-text result))))
+
+(defn start-parinfer [ed]
+  (let [cm (editor/->cm-ed ed)
+        old-txt (.getValue cm)
+        result (.parenMode pi old-txt)]
+    (if (.-success result)
+      (update-editor-and-state ed cm result) ; Is balanced - just adjust editor
+      (popup/popup! {:header "Unbalanced Parenthesis"
+                     :body [:div
+                              [:p "The opened file is unbalanced. Parinfer is disabled"]
+                              [:p "Please, correct the file and activate parinfer again"]]
+                     :buttons [{:label "Ok"}]}))))
+
+(defn start-or-continue-parinfer []
+  (let [ed (pool/last-active)]
+    (if (-> @parinfer-editors (get-in [:modes (editor-id)]) nil?)
+      (start-parinfer ed))))
 
 (behavior ::start-parinfer
           :triggers #{:active}
-          :reaction (fn [obj]
-                      (println "Activated editor")))
+          :reaction (fn [obj] (start-or-continue-parinfer)))
 
 (behavior ::stop-parinfer
           :triggers #{:closed}
           :reaction (fn [obj]
-                      (println "Deactivated editor")))
+                      (object/update!
+                        parinfer-editors
+                        [:modes]
+                        dissoc (editor-id))))
+
+;; Statusbar Updates
+(behavior ::update-parinfer-mode
+          :triggers #{:active}
+          :reaction (fn [obj]
+                      (object/assoc-in! parinfer-editors [:current-editor] (editor-id))))
+
+;; Commands
+(cmd/command {:command :parinfer-behind-cursor
+              :desc "Parinfer: infer parenthesis on current editor"
+              :exec parinfer-behind-cursor})
+
+(cmd/command {:command :parinfer-enable
+              :desc "Parinfer: enable parinfer in current editor"
+              :exec (fn [_]
+                      (object/assoc-in!
+                              parinfer-editors
+                              [:modes (editor-id)] nil)
+                      (start-or-continue-parinfer))})
+
+(cmd/command {:command :parinfer-disable
+              :desc "Parinfer: disable parinfer in current editor"
+              :exec (fn [_] (object/assoc-in!
+                              parinfer-editors
+                              [:modes (editor-id)] :disabled))})
+
+(cmd/command {:command :parinfer-toggle-mode
+              :desc "Parinfer: toggle mode (indent or paren) in current editor"
+              :exec (fn [_]
+                      (let [e (editor-id)]
+                        (case (-> @parinfer-editors (get-in [:modes (editor-id)]))
+                          :indent (object/assoc-in! parinfer-editors [:modes e] :paren)
+                          :paren (object/assoc-in! parinfer-editors [:modes e] :indent)
+                          nil)))})
